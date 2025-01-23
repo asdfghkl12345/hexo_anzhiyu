99a1c50665f103a171724bbd0554e3b6272eead3
asdfghkl12345 <wangsishun201212@outlook.com>
2025-01-23 20:53:11 +0800

    Site updated: 2025-01-23 20:52:49

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/14605/","posts/39417/","posts/37773/","posts/21712/","posts/17489/"];function toRandomPost(){
+var posts=["posts/14605/","posts/37773/","posts/21712/","posts/39417/","posts/17489/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
