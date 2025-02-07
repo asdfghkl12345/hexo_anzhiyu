d0994fa808da2d3591435486afc5b3e05b553bad
asdfghkl12345 <wangsishun201212@outlook.com>
2025-02-07 17:19:59 +0800

    Site updated: 2025-02-07 17:19:55

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,4 +1,4 @@
-var posts=["posts/14605/","posts/37773/","posts/21712/","posts/39417/","posts/17489/"];function toRandomPost(){
+var posts=["posts/14605/","posts/37773/","posts/39417/","posts/21712/","posts/17489/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };var friend_link_list=[];
     var refreshNum = 1;
